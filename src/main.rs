15777c7ed2c865b70adde7c97f4465cef816df2f
justina colmena ~biz <justina@colmena.biz>
2022-12-20 00:32:58 -0900

    working hello world code -- pulling in dependencies that will be needed

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+
+use rocket::{
+	Rocket,
+	get,
+	routes,
+	launch,
+};
+use rocket_session_store::{
+	memory::MemoryStore,
+	SessionStore,
+	SessionResult,
+	Session,
+	CookieConfig,
+};
+
+
+use dotenvy::dotenv;
+use std::env;
+
+
+use rocket::tokio::time::{sleep, Duration};
+use rocket::response::content;
+use comrak::{markdown_to_html, ComrakOptions};
+use bbscope::BBCode;
+
+
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let memory_store: MemoryStore::<String> = MemoryStore::default();
+	let store: SessionStore<String> = SessionStore {
+		store: Box::new(memory_store),
+		name: "token".into(),
+		duration: Duration::from_secs(3600 * 24 * 3),
+		// The cookie config is used to set the cookie's path and other options.
+		cookie: CookieConfig::default(),
+	};
+   let _rocket = rocket::build().attach(store.fairing())
+        .mount("/", routes![index])
+        .launch()
+        .await?;
+
+    Ok(())
+}
+
+#[get("/")]
+async fn index(session: Session<'_, String>) -> SessionResult<String> {
+	let name: Option<String> = session.get().await?;
+	if let Some(name) = name {
+		Ok(format!("Hello, {}!", name))
+	} else {
+		Ok("Hello, world!".into())
+	}
+}
+
