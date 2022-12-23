1cd15a15712d73c957dfc9ba720e4e8260539296
justina colmena ~biz <justina@colmena.biz>
2022-12-23 09:19:26 -0900

    set status & content type of response

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # farmgate
 A CMS with user auth, blog, wiki, and discussion to run on PostgreSQL
+
+Configure `DATABASE_URL` and `ROCKET_SECRET_KEY` in the file `./.env`
+
+**.env**
+
+```
+DATABASE_URL = postgres://username:password@localhost/somedatabase
+ROCKET_SECRET_KEY = qjsVtkPNv7903JcCwbSxNl68cMT9F8D6h834RoDNSSI=
+```
+The variable `ROCKET_SECRET_KEY` may be set to the output of the
+following command to generate a new secret key
+
+```
+$ head -c32 /dev/random | base64
+```
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,52 @@
 use dotenvy::dotenv;
-use std::{env, error, fmt};
-use rand::prelude::*;
-use rand::{Rng, thread_rng};
 use rand::distributions::{Alphanumeric, DistString};
-
-use rocket::{Rocket, get, routes, launch};
-use rocket::tokio::time::{sleep, Duration};
-use rocket::response::{status, content, Responder};
+use rocket::{get, routes};
+use rocket::tokio::time::{Duration};
+use rocket::http::{Status, ContentType};
 use rocket_session_store::{memory::MemoryStore, SessionStore,
 	SessionResult, Session, CookieConfig};
+use tokio_postgres::{NoTls};
 
-use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio_postgres::{NoTls, Error};
-
-use comrak::{markdown_to_html, ComrakOptions};
-use bbscope::BBCode;
+// use comrak::{markdown_to_html, ComrakOptions};
+// use bbscope::BBCode;
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
 	let _path_to_dot_env = dotenv().ok();
 
-    let memory_store: MemoryStore::<String> = MemoryStore::default();
+	let memory_store: MemoryStore::<String> = MemoryStore::default();
 	let store: SessionStore<String> = SessionStore {
 		store: Box::new(memory_store),
 		name: "token".into(),
 		duration: Duration::from_secs(3600),
-		// The cookie config is used to set the cookie's path and other options.
 		cookie: CookieConfig::default(),
 	};
-   let _rocket = rocket::build().attach(store.fairing())
-        .mount("/", routes![index])
-        .launch()
-        .await?;
-
-    Ok(())
+	let _rocket = rocket::build().attach(store.fairing())
+		.mount("/", routes![index])
+		.launch()
+		.await?;
+	Ok(())
 }
 
 #[get("/")]
 async fn index(session: Session<'_, String>)
-		-> SessionResult<content::RawHtml<String>>
-{
-	let session_id: String = session.get().await
-		.unwrap_or_default()
-		.and_then(|name| if name.len() == 54
-			&& name.starts_with("jK")
-			&& name.bytes().all(|x| x.is_ascii_alphanumeric())
-			{Some(name)} else {None})
-		.unwrap_or("jK".to_string()
-		+ &Alphanumeric.sample_string(&mut rand::thread_rng(), 52));
+			-> SessionResult<(Status, (ContentType, String))> {
+	let session_id: String = session.get().await.unwrap_or_default()
+		.and_then(|name| if name.len() == 54 && name.bytes()
+		.all(|x| x.is_ascii_alphanumeric()) {Some(name)} else {None})
+		.unwrap_or(Alphanumeric.sample_string(&mut rand::thread_rng(), 54));
 	session.set(session_id.clone()).await?;
 
 	let database_url = std::env::var("DATABASE_URL")
 		.unwrap_or("localhost".to_string());
 
-	let mut conn_error_string = "".to_string();
+	let mut database_error = "".to_string();
 	let Ok((client, connection))
 		= tokio_postgres::connect(&database_url, NoTls).await
 		.or_else(|e: tokio_postgres::error::Error|
-			{conn_error_string += &e.to_string(); Err(e)})
-	else {return Ok(content::RawHtml(format!(
-		"database connection failed: {}", conn_error_string )))};
+			{database_error += &e.to_string(); Err(e)})
+	else {return Ok((Status::new(500), (ContentType::Plain,
+		format!("database connection failed: {}", database_error))))};
 
 	tokio::spawn(async move {
 		if let Err(e) = connection.await {
@@ -68,11 +54,16 @@ async fn index(session: Session<'_, String>)
 		}});
 
 	let Ok(rows) = client
-        .query("SELECT $1::TEXT, NOW()::TEXT", &[&"hello world"]).await
-	else {return Ok(content::RawHtml("database query failed".to_string()))};
+        .query("SELECT $1::TEXT, $2::TEXT, NOW()::TEXT",
+			&[&&session_id, &"hello world"]).await
+	.or_else(|e: tokio_postgres::error::Error|
+			{database_error += &e.to_string(); Err(e)})
+	else {return Ok((Status::new(500), (ContentType::Plain,
+		format!("database connection failed: {}", database_error))))};
 
 	// panics if rows[].get() aren't the right type
-    Ok(content::RawHtml(format!("{}<br>{} {}", session_id,
-		rows[0].get::<usize, String>(0), rows[0].get::<usize, String>(1))))
+    Ok((Status::Ok,(ContentType::HTML, format!("{}<br>{} {}",
+		rows[0].get::<usize, String>(0), rows[0].get::<usize, String>(1),
+			rows[0].get::<usize, String>(2)))))
 }
 
